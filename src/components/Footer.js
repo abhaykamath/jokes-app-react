53818d681add67aa63b835b0631774e30f25ad83
Abhay Kamath <abhaykamath2299@gmail.com>
2023-08-24 17:59:05 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div id="footer">
+      Created by M Abhay Kamath with ReactJs
+      <i className="fa-brands fa-react" style={{ color: "#16d4be" }}></i>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "../styles/Main.css";
+
+const API_URL = "https://official-joke-api.appspot.com/random_joke";
+
+function Main() {
+  const [joke, setJoke] = useState("");
+  const [loader, setLoader] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
+
+  async function getJoke() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    setBookmarked((bookmarked) => !bookmarked);
+    setLoader(false);
+    setJoke(data);
+  }
+
+  return (
+    <div id="main">
+      <div className="card">
+        <i
+          className="fa-solid fa-bookmark bookmark"
+          style={{
+            color: bookmarked ? "#272829" : "#ccc",
+          }}
+        ></i>
+        <h2>Jokes</h2>
+        {joke ? (
+          <p>
+            <i>
+              <strong>{joke.setup}</strong>
+            </i>{" "}
+            - {joke.punchline}
+          </p>
+        ) : (
+          <p>
+            Let's have a laugh. Click on the '<strong>new joke</strong>' button.
+          </p>
+        )}
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            setLoader(true);
+            getJoke();
+          }}
+        >
+          new joke {loader && <div className="loader"></div>}
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            setBookmarked((bookmarked) => !bookmarked);
+          }}
+        >
+          bookmark
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
